65fe6770d7416f0d0ffd170fed6d0eadd0ec7467
Not-Darshil <darshilkumar0353@gmail.com>
2023-07-05 02:14:27 +0530

    commit and push done

--- start.py
+++ start.py
@@ -1,1 +1,11 @@
-import kivy
+STEP-1 CREATE A APP
+STEP-1 CREATE Game
+STEP-1 Build the Game
+STEP-1 Run The app
+
+
+from kivy.app import App
+class StockSnap(App):
+    def build(self):
+        return
+
