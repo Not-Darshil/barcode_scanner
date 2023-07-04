3739f44ca6856ccadb97e0a0de8dafa92e12027a
Not-Darshil <darshilkumar0353@gmail.com>
2023-07-05 02:22:09 +0530

    commit and push done

--- start.py
+++ start.py
@@ -1,11 +1,18 @@
-STEP-1 CREATE A APP
-STEP-1 CREATE Game
-STEP-1 Build the Game
-STEP-1 Run The app
+# STEP-1 CREATE A APP
+# STEP-1 CREATE Game
+# STEP-1 Build the Game
+# STEP-1 Run The app
 
 
 from kivy.app import App
+from kivy.uix.widget import Widget
+
+class Working(Widget):
+    pass
+
 class StockSnap(App):
     def build(self):
-        return
+        return Working()
+
+StockSnap().run()
 
